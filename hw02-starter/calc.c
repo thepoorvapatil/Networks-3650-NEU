ddf3c66ba434c9e302224a7c392728e4087294a0
Poorva Patil <poorvapatil@Poorvas-MacBook-Pro.local>
2020-01-21 16:22:40 -0500

    calc.c initial commit, changes to if condition

--- hw02-starter/calc.c
+++ hw02-starter/calc.c
@@ -1,1 +1,29 @@
 #include<stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    if(argc != 4 || (strncmp(argv[2], "+", 8)!=0 && strncmp(argv[2], "-", 8)!=0 && strncmp(argv[2], "*", 8)!=0 && strncmp(argv[2], "/", 8)!=0 ) ){
+        puts("Usage:\n./ccalc N op N");
+        return 1;
+    }
+
+    int xx = atoi(argv[1]);
+    int yy = atoi(argv[3]);
+    char * fmt = "%d %s %d = %d\n";
+
+    if(!strncmp(argv[2], "+", 8))
+        printf(fmt, xx, argv[2], yy, xx + yy);
+
+    if(!strncmp(argv[2], "-", 8))
+        printf(fmt, xx, argv[2], yy, xx - yy);
+    
+    if(!strncmp(argv[2], "*", 8))
+        printf(fmt, xx, argv[2], y, xx * yy);
+    
+    if(!strncmp(argv[2], "/", 8))
+        printf(fmt, xx, argv[2], yy, xx / yy);
+
+    return 0;
+}
